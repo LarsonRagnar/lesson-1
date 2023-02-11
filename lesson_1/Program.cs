ce5285ae82c2692e1f97ec1524b6245213f824fd
LarsonRagnar <larionov1992@gmail.com>
2023-02-11 18:57:41 +0300

    квадрат числа

--- lesson_1/Program.cs
+++ lesson_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+Console.WriteLine("Введите число, для которого мы ищем квадрат: ");
+int number =Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine("Квадрат от числа: " + number + " равен " + result);
+
